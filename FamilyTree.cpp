97d3f1bd23c5d090e5742fa6c18e25c1937087d4
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2020-04-16 23:24:27 +0300

    near final

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -5,8 +5,9 @@ using namespace family;
 
 node::node(string name)
 {
-    this->name=name;
+    this->child=name;
     this->father=this->mother=nullptr;
+    this->gender="";
 }
 Tree::Tree(string name)
 {
@@ -22,7 +23,7 @@ node* Tree::search_name(node* root,string name)
 {
     if(root==NULL)
         return nullptr;
-    if(root->name.compare(name)==0)
+    if(root->child.compare(name)==0)
         return root;
     //Else - return the first one to find
     node* result_from_fatherSide=search_name(root->father,name);
@@ -44,35 +45,148 @@ node* Tree::search_name(node* root,string name)
 Tree& Tree::addFather(string child,string father)
 {
     node* node_child=search_name(child);
-    if(node_child!=nullptr)
+    if(node_child==nullptr)//If the given name does not exist in the tree
+        throw("The tree doesn't contain the name: %s",child);
+    if(node_child->father==NULL)//If the given name exists, but has no father - add the new father
+    {
         node_child->father=new node(father);
-    
-    return *this;
-    
+        node_child->father->gender="male"; 
+        return *this;
+    }
+    else
+        throw("%s already has a father, his name is: %s",child,node_child->father->child);
 }
 Tree& Tree::addMother(string child,string mother)
 {
     node* node_child=search_name(child);
-    if(node_child!=nullptr)
+    if(node_child==nullptr)//If the given name does not exist in the tree
+        throw("The tree doesn't contain the name: %s",child);
+    if(node_child->mother==NULL)//If the given name exists, but has no mother - add the new mother
+    {
         node_child->mother=new node(mother);
-    
-    return *this;
+        node_child->mother->gender="female";
+        return *this; 
+    }
+    else
+        throw("%s already has a mother, her name is: %s",child,node_child->mother->child);
 }
 
+
+
+
 /*----------------------------------*/
-string Tree::relation(string name)
+//Relation section
+
+//Source: GeeksForGeeks
+int Tree::level(node*root,string data,int level)
 {
-    return "";
+    if(root==NULL)
+        return 0;
+    if(root->child.compare(data)==0)
+        return level;
+    
+    int down=Tree::level(root->mother,data,level+1);
+    if(down!=0)
+        return down;
+    
+    //Else, we need to return level of father
+    down=Tree::level(root->father,data,level+1);
+    return down;
+}
+
+string Tree::relation(string name)//Return relation of the given name, starting from the root of the tree
+{
+    if(search_name(name)==NULL);
+        return "unrelated";
+    
+    if(this==NULL)
+        return NULL;
+    
+    else
+    {
+        int level=Tree::level(this->root,name,1);
+        node*found=search_name(name);
+        if(level!=1&&found!=NULL)
+            return "me";
+        
+        if(found->gender=="male")
+        {
+            if(level==2)
+                return "father";
+            if(level==3)
+                return "grandfather";
+
+            //If we reached here, we need to count great---> ...great grand
+            string ans="grandfather";
+            for(int i=0;i<level-3;++i)
+                ans.insert(0,"great-");
+            return ans;
+        }
+
+        else if(found->gender=="female")
+        {
+            if(level==2)
+                return "mother";
+            if(level==3)
+                return "grandmother";
+
+            //If we reached here, we need to count great---> ...great grand
+            string ans="grandmother";
+            for(int i=0;i<level-3;++i)
+                ans.insert(0,"great-");
+            return ans;
+        }
+
+    }
+    return "unrelated";
+    
+}
+node* Tree::findRelate(node*root,string relate)
+{
+    if(root==NULL)
+        return NULL;
+    if(Tree::relation(root->child).compare(relate)==0)
+        return root;
+    
+    node* mother_side=findRelate(root->mother,relate);
+    node* father_side=findRelate(root->father,relate);
+
+    if(mother_side!=NULL)
+        return mother_side;
+    if(father_side!=NULL)
+        return father_side;
+    return NULL;
+
 }
 string Tree::find(string name)
 {
     //need to return relation
-    return "";
+    if(this==NULL)
+        return "NULL";
+    node* found=findRelate(this->root,name);
+    if(found==NULL)
+        throw("There is no relation: "+name);
+    return found->child;
 }
 
+
+/*----------------------------------*/
+//Displaying tree
+//Source: Stackoverflow
+void Tree::displayTree(const std::string& prefix,node*node,bool isLeft)
+{
+    if( node != nullptr )
+    {
+        cout << prefix;
+        cout << (isLeft ? "├──" : "└──" );
+        cout << node->child << endl;
+        displayTree( prefix + (isLeft ? "│   " : "    "), node->mother, true);
+        displayTree( prefix + (isLeft ? "│   " : "    "), node->father, false);
+    }
+}
 void Tree::display()
 {
-
+    displayTree("",this->root,false);
 }
 
 
@@ -92,7 +206,7 @@ node* remove_node_from_tree(node* root, string name)
 {
     if(root==NULL)
         return NULL;
-    if(root->name.compare(name)==0)//Found the relevant person - and it's the temporary root
+    if(root->child.compare(name)==0)//Found the relevant person - and it's the temporary root
     {
         free_node(root);
         return NULL;
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -9,22 +9,30 @@ namespace family
 {
     class node
     {
+        
         public:
-        string name;
+        string child;
         node*father;
         node*mother;
+        string gender;
         node(string name);
 
     };
     class Tree
     { 
+        private:
+            void free_node(node* person);
+            node* remove_node_from_tree(node* root, string name);
+            node* findRelate(node*n,string relate);
+            void displayTree(const std::string& prefix,node*root,bool isLeft);
+            int level(node*node,string data,int level);
         public:
             node*root;
             Tree(string name);
             /*add father to the given name in the tree*/
-            Tree& addFather(string name, string father);
+            Tree& addFather(string child, string father);
             /*add mother to the given name in the tree*/
-            Tree& addMother(string name, string mother);
+            Tree& addMother(string child, string mother);
             /*displays the tree in a human-friendly format*/
             void display();
             /*returns the relation of given name in the tree
